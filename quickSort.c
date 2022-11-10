6454b4a64aeb434a599ad9d91143650a705d63b0
Vidur Kaushik <54384846+VidurK2@users.noreply.github.com>
2022-11-10 14:08:17 +0530

    Update quickSort.c

--- quickSort.c
+++ quickSort.c
@@ -1,56 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int partTest(int *Arr, int first, int stop){
-    int pivot = Arr[first];
-    int i = first+1;
-    int j = stop;
-
-    while (i <= j){
-        if (Arr[i] > pivot){
-//            swap (Arr, i, j);
-            j--;
-        }
-        else {
-            i++;
-        }
-    }
-//    swap (Arr, first, j);
-    return j;
-}
-
-int partition(int *Arr, int first, int last){
-    int pivot = Arr[last];
-    int ind = first;
-    for (int i = first; i < last; i++){
-        if(Arr[i] <= pivot){
-            int temp = Arr[i];
-            Arr[i] = Arr[ind];
-            Arr[ind] = temp;
-            ind++;
-        }
-    }
-    int temp = Arr[ind];
-    Arr[ind] = Arr[last];
-    Arr[last] = temp;
-    return ind;
-}
-
-void quickSort(int *Arr, int first, int last){
-    if(Arr && first < last){
-        int ind = partition(Arr, first, last);
-        quickSort(Arr, first, ind-1);
-        quickSort(Arr, ind+1, last);
-    }
-}
-
-int main(){
-    int Arr[] = {1, 2, 3, 4, 10, 7, 33};
-    int n = sizeof(Arr)/sizeof(Arr[0]);
-    quickSort(Arr, 0, n-1);
-    for (int i = 0; i < n; i++){
-        printf("%d ", Arr[i]);
+int quickPart(int A[], int start, int end) {
+    int pivot = A[end]; //Setting the last element as the pivot
+    
+    //Traversing through the array from start to end
+    for (int i=start; i<end; i++) {
         
-    return 0;
+        //If ith element is less than pivot then we swap and move one elment forward
+        if (A[i] <= pivot) {
+            int x = A[i];
+            A[i] = A[start];
+            A[start] = x;
+            start++;
+        }
     }
+    
+    //We swap the first and last element before returning the position to the quick sort function
+    int x = A[start];
+    A[start] = A[end];
+    A[end] = x;
+    return start;
+}
+
+
+//Function to do quick sort on array
+int quickSortArr(int A[], int start, int end) {
+    
+    //Run as long as the first element is smaller than the last element
+    if (start < end) {
+        int pos = quickPart(A, start, end); //Taking out middle element pos
+        
+        //Recursing from start to middle and middle to last
+        quickSortArr(A, start, pos-1);
+        quickSortArr(A, pos+1, end);
+    }
+    
+    
+    return 0;
+}
+
+//Function to print out the array
+int printArr(int A[], int num) {
+    for (int i=0; i<num; i++) {
+        int x = A[i];
+        printf("%d \t", x);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+//Main function that creates the array with input
+int main () {
+    int num;
+    printf("Enter number of elements : ");
+    scanf("%d", &num);
+    
+    int A[num];
+    
+    for (int i=0; i<num; i++) {
+        int x;
+        printf("Enter element : ");
+        scanf("%d", &x);
+        
+        A[i] = x;
+    }
+    
+    quickSortArr(A, 0, num-1);
+    
+    printf("========Sorted List=========\n");
+    printArr(A, num);
+    
+    return 0;
 }
